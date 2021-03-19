4b900b19341712c8841a7d1ee6a9f9686ee2adca
Chaitanya Chandrika Raghuvanshi <70335336+Chaitanyaraghuvanshi@users.noreply.github.com>
2021-03-19 14:38:33 +0530

    Update task1-GRIP.py

--- task1-GRIP.py
+++ task1-GRIP.py
@@ -22,6 +22,7 @@ plt.xlabel('Hours Studied')
 plt.ylabel('Percentage Score')  
 plt.show()
 print('The above plot shows high correlation between hours studied and percentage scored.')
+
 #Splitting the data into training and testing set 
 x_train, x_test, y_train, y_test = train_test_split(x, y,train_size=0.8, 
                             test_size=0.2, random_state=10) 
@@ -30,10 +31,28 @@ reg_model = LinearRegression()
 reg_model.fit(x_train, y_train) 
 
  # Predicting the scores
-y_pred = regressor.predict(X_test)
+y_pred = reg_model.predict(x_test)
 
 #Plotting the predicted scores vs the testing scores
 plt.scatter(x_test,y_test)
+plt.plot(x_test,y_pred,'r')
+plt.legend(['Predicted Data','Testing Data'])
+plt.xlabel('Hours Studied')  
+plt.ylabel('Percentage Score')
+plt.show()
+
+# Comparing Actual vs Predicted
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})  
+print(df) 
+
+hours = 9.25
+own_pred = reg_model.predict([[hours]])
+print("No of Hours = {}".format(hours))
+print("Predicted Score = {}".format(own_pred[0]))
+
+#In order to evaluate the performance 
+print('Mean Absolute Error:', 
+      metrics.mean_absolute_error(y_test, y_pred)) 
 plt.plot(x_test,y_pred,'r')
 plt.legend(['Predicted Data','Testing Data'])
 plt.xlabel('Hours Studied')  
